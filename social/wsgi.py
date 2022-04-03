620c32cea775741dc4e758f9cce9e4d21242cd9d
mn9142001 <75080962+mn9142001@users.noreply.github.com>
2022-04-03 08:23:57 +0200

    Update wsgi.py

--- social/wsgi.py
+++ social/wsgi.py
@@ -1,19 +1,6 @@
-"""
-WSGI config for social project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social.settings')
-
 application = get_wsgi_application()
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application, root="/staticfiles")
